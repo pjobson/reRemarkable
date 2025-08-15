bd86fc24fc37c90e352d28ce611d0b5e49eac237
Paul Jobson <pjobson@gmail.com>
2025-08-15 01:04:00 -0400

    Renamed project.

--- run.sh
+++ run.sh
@@ -4,26 +4,26 @@ foldername=`dirname $(readlink -f $0)`
 PYTHON="$foldername/venv/bin/python3"
 PIP="$foldername/venv/bin/pip"
 
-if [ ! -f "$foldername/venv" ]; then
-    python3 -m venv venv
-fi
+# if [ ! -d "$foldername/venv" ]; then
+#     python3 -m venv "$foldername/venv"
+# fi
 
-# Check if requirements are installed
-if [ -f "$foldername/requirements.txt" ]; then
-   # Check if all required packages are installed
-   missing_packages=0
-   while read -r package; do
-       if [ -n "$package" ] && ! $PIP freeze | grep -qi "^${package%%[>=<]*}"; then
-           missing_packages=1
-           break
-       fi
-   done < "$foldername/requirements.txt"
+# # Check if requirements are installed
+# if [ -f "$foldername/requirements.txt" ]; then
+#    # Check if all required packages are installed
+#    missing_packages=0
+#    while read -r package; do
+#        if [ -n "$package" ] && ! $PIP freeze | grep -qi "^${package%%[>=<]*}"; then
+#            missing_packages=1
+#            break
+#        fi
+#    done < "$foldername/requirements.txt"
 
-   if [ $missing_packages -eq 1 ]; then
-       echo "Installing missing requirements..."
-       $PIP install -r "$foldername/requirements.txt"
-   fi
-fi
+#    if [ $missing_packages -eq 1 ]; then
+#        echo "Installing missing requirements..."
+#        $PIP install -r "$foldername/requirements.txt"
+#    fi
+# fi
 
 export PYTHONPATH=$foldername/reremarkable:$foldername/reremarkable_lib:$foldername
 exec $PYTHON $foldername/bin/reremarkable "$@"
