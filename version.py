53519a2a11055de4a0036eb6f48df43ea316f9bd
Paul Jobson <pjobson@gmail.com>
2025-08-18 05:32:37 -0400

    moved version info to file

---
+++ version.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+
+"""
+Version information for reRemarkable
+
+This file contains the single source of truth for the application version.
+"""
+
+__version__ = '2.2'
+__license__ = 'MIT'
+
+def get_version():
+    """Return the version string."""
+    return __version__
+
+def get_license():
+    """Return the license string."""
+    return __license__
